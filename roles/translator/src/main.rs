a88ebed1068c93dce92050eea44f058ddc8fec0c
lorban <lorenzo.bonax@gmail.com>
2024-06-10 22:56:16 +0200

    TProxy restart if gests disconnected by upstream
    - Add `start` function and put starting logic there.
    - Every AbortHandle of each task is stored in a collector, which is a
    mutex.
    - Added `kill_tasks` function that takes in input this mutex, access
    it, pop each JoinHandle and kill the corresponding task.
    
    When receive an UpstreamShutdown does the following:
    1. waits a random amount of time s 0<s<3 secs (if there 100000 TProxies,
    then 100000 proxies will connect again _at the same time_).
    2. calls `kill_tasks`
    3. calls `start`
    
    use tokio::task in favor of async_std::task

--- roles/translator/src/main.rs
+++ roles/translator/src/main.rs
@@ -3,24 +3,28 @@ mod args;
 mod lib;
 
 use args::Args;
+use async_channel::{bounded, unbounded, Receiver, Sender};
+use downstream_sv1::DownstreamMessages;
 use error::{Error, ProxyResult};
+use futures::{select, FutureExt};
 use lib::{downstream_sv1, error, proxy, proxy_config, status, upstream_sv2};
 use proxy_config::ProxyConfig;
-use roles_logic_sv2::utils::Mutex;
-
-use async_channel::{bounded, unbounded};
-use futures::{select, FutureExt};
+use rand::Rng;
+use roles_logic_sv2::{
+    mining_sv2::{ExtendedExtranonce, NewExtendedMiningJob, SetNewPrevHash, SubmitSharesExtended},
+    utils::Mutex,
+};
 use std::{
     net::{IpAddr, SocketAddr},
     str::FromStr,
     sync::Arc,
 };
 
-use tokio::{sync::broadcast, task};
+use tokio::{sync::broadcast, task, task::AbortHandle, time::Duration};
 use v1::server_to_client;
 
 use crate::status::{State, Status};
-use tracing::{debug, error, info};
+use tracing::{debug, error, info, warn};
 /// Process CLI args, if any.
 #[allow(clippy::result_large_err)]
 fn process_cli_args<'a>() -> ProxyResult<'a, ProxyConfig> {
@@ -43,7 +47,7 @@ async fn main() {
         Ok(p) => p,
         Err(e) => panic!("failed to load config: {}", e),
     };
-    info!("PC: {:?}", &proxy_config);
+    info!("Proxy Config: {:?}", &proxy_config);
 
     let (tx_status, rx_status) = unbounded();
 
@@ -77,6 +81,129 @@ async fn main() {
         broadcast::Receiver<server_to_client::Notify>,
     ) = broadcast::channel(10);
 
+    let task_collector: Arc<Mutex<Vec<(AbortHandle, String)>>> = Arc::new(Mutex::new(Vec::new()));
+
+    start(
+        rx_sv2_submit_shares_ext.clone(),
+        tx_sv2_submit_shares_ext.clone(),
+        tx_sv2_new_ext_mining_job.clone(),
+        tx_sv2_set_new_prev_hash.clone(),
+        tx_sv2_extranonce.clone(),
+        rx_sv2_extranonce.clone(),
+        rx_sv2_set_new_prev_hash.clone(),
+        rx_sv2_new_ext_mining_job.clone(),
+        rx_sv1_downstream.clone(),
+        tx_sv1_bridge.clone(),
+        tx_sv1_notify.clone(),
+        target.clone(),
+        tx_status.clone(),
+        task_collector.clone(),
+        proxy_config.clone(),
+    )
+    .await;
+
+    debug!("Starting up signal listener");
+    let task_collector_ = task_collector.clone();
+
+    let mut interrupt_signal_future = Box::pin(tokio::signal::ctrl_c().fuse());
+    debug!("Starting up status listener");
+    // Check all tasks if is_finished() is true, if so exit
+    loop {
+        let task_status = select! {
+            task_status = rx_status.recv().fuse() => task_status,
+            interrupt_signal = interrupt_signal_future => {
+                match interrupt_signal {
+                    Ok(()) => {
+                        info!("Interrupt received");
+                    },
+                    Err(err) => {
+                        error!("Unable to listen for interrupt signal: {}", err);
+                        // we also shut down in case of error
+                    },
+                }
+                break;
+            }
+        };
+        let task_status: Status = task_status.unwrap();
+
+        match task_status.state {
+            // Should only be sent by the downstream listener
+            State::DownstreamShutdown(err) => {
+                error!("SHUTDOWN from: {}", err);
+                break;
+            }
+            State::BridgeShutdown(err) => {
+                error!("SHUTDOWN from: {}", err);
+                break;
+            }
+            State::UpstreamShutdown(err) => {
+                error!("SHUTDOWN from: {}", err);
+
+                // wait a random amount of time between 0 and 3000ms
+                // if all the downstreams try to reconnect at the same time, the upstream may fail
+                tokio::time::sleep(Duration::from_millis(1000)).await;
+                let mut rng = rand::thread_rng();
+                let wait_time = rng.gen_range(0..=3000);
+                tokio::time::sleep(Duration::from_millis(wait_time)).await;
+
+                // kill al the tasks
+                let task_collector_aborting = task_collector_.clone();
+                kill_tasks(task_collector_aborting.clone());
+
+                error!("Trying recconnecting to upstream");
+                start(
+                    rx_sv2_submit_shares_ext.clone(),
+                    tx_sv2_submit_shares_ext.clone(),
+                    tx_sv2_new_ext_mining_job.clone(),
+                    tx_sv2_set_new_prev_hash.clone(),
+                    tx_sv2_extranonce.clone(),
+                    rx_sv2_extranonce.clone(),
+                    rx_sv2_set_new_prev_hash.clone(),
+                    rx_sv2_new_ext_mining_job.clone(),
+                    rx_sv1_downstream.clone(),
+                    tx_sv1_bridge.clone(),
+                    tx_sv1_notify.clone(),
+                    target.clone(),
+                    tx_status.clone(),
+                    task_collector_.clone(),
+                    proxy_config.clone(),
+                )
+                .await;
+            }
+            State::Healthy(msg) => {
+                info!("HEALTHY message: {}", msg);
+            }
+        }
+    }
+}
+
+fn kill_tasks(task_collector: Arc<Mutex<Vec<(AbortHandle, String)>>>) {
+    let _ = task_collector.safe_lock(|t| {
+        while let Some(handle) = t.pop() {
+            handle.0.abort();
+            warn!("Killed task: {:?}", handle.1);
+        }
+    });
+}
+
+#[allow(clippy::too_many_arguments)]
+async fn start<'a>(
+    rx_sv2_submit_shares_ext: Receiver<SubmitSharesExtended<'static>>,
+    tx_sv2_submit_shares_ext: Sender<SubmitSharesExtended<'static>>,
+    tx_sv2_new_ext_mining_job: Sender<NewExtendedMiningJob<'static>>,
+    tx_sv2_set_new_prev_hash: Sender<SetNewPrevHash<'static>>,
+    tx_sv2_extranonce: Sender<(ExtendedExtranonce, u32)>,
+    rx_sv2_extranonce: Receiver<(ExtendedExtranonce, u32)>,
+    rx_sv2_set_new_prev_hash: Receiver<SetNewPrevHash<'static>>,
+    rx_sv2_new_ext_mining_job: Receiver<NewExtendedMiningJob<'static>>,
+    rx_sv1_downstream: Receiver<DownstreamMessages>,
+    tx_sv1_bridge: Sender<DownstreamMessages>,
+    tx_sv1_notify: broadcast::Sender<server_to_client::Notify<'static>>,
+    target: Arc<Mutex<Vec<u8>>>,
+    tx_status: async_channel::Sender<Status<'static>>,
+    task_collector: Arc<Mutex<Vec<(AbortHandle, String)>>>,
+    proxy_config: ProxyConfig,
+) {
     // Format `Upstream` connection address
     let upstream_addr = SocketAddr::new(
         IpAddr::from_str(&proxy_config.upstream_address)
@@ -85,7 +212,7 @@ async fn main() {
     );
 
     let diff_config = Arc::new(Mutex::new(proxy_config.upstream_difficulty_config.clone()));
-
+    let task_collector_upstream = task_collector.clone();
     // Instantiate a new `Upstream` (SV2 Pool)
     let upstream = match upstream_sv2::Upstream::new(
         upstream_addr,
@@ -98,6 +225,7 @@ async fn main() {
         status::Sender::Upstream(tx_status.clone()),
         target.clone(),
         diff_config.clone(),
+        task_collector_upstream,
     )
     .await
     {
@@ -107,12 +235,12 @@ async fn main() {
             return;
         }
     };
-
+    let task_collector_init_task = task_collector.clone();
     // Spawn a task to do all of this init work so that the main thread
     // can listen for signals and failures on the status channel. This
     // allows for the tproxy to fail gracefully if any of these init tasks
     //fail
-    task::spawn(async move {
+    let task = task::spawn(async move {
         // Connect to the SV2 Upstream role
         match upstream_sv2::Upstream::connect(
             upstream.clone(),
@@ -152,6 +280,7 @@ async fn main() {
             async_std::task::sleep(std::time::Duration::from_millis(100)).await;
         }
 
+        let task_collector_bridge = task_collector_init_task.clone();
         // Instantiate a new `Bridge` and begins handling incoming messages
         let b = proxy::Bridge::new(
             rx_sv1_downstream,
@@ -163,6 +292,7 @@ async fn main() {
             extended_extranonce,
             target,
             up_id,
+            task_collector_bridge,
         );
         proxy::Bridge::start(b.clone());
 
@@ -172,6 +302,7 @@ async fn main() {
             proxy_config.downstream_port,
         );
 
+        let task_collector_downstream = task_collector_init_task.clone();
         // Accept connections from one or more SV1 Downstream roles (SV1 Mining Devices)
         downstream_sv1::Downstream::accept_connections(
             downstream_addr,
@@ -181,49 +312,8 @@ async fn main() {
             b,
             proxy_config.downstream_difficulty_config,
             diff_config,
+            task_collector_downstream,
         );
     }); // End of init task
-
-    debug!("Starting up signal listener");
-    let mut interrupt_signal_future = Box::pin(tokio::signal::ctrl_c().fuse());
-    debug!("Starting up status listener");
-
-    // Check all tasks if is_finished() is true, if so exit
-    loop {
-        let task_status = select! {
-            task_status = rx_status.recv().fuse() => task_status,
-            interrupt_signal = interrupt_signal_future => {
-                match interrupt_signal {
-                    Ok(()) => {
-                        info!("Interrupt received");
-                    },
-                    Err(err) => {
-                        error!("Unable to listen for interrupt signal: {}", err);
-                        // we also shut down in case of error
-                    },
-                }
-                break;
-            }
-        };
-        let task_status: Status = task_status.unwrap();
-
-        match task_status.state {
-            // Should only be sent by the downstream listener
-            State::DownstreamShutdown(err) => {
-                error!("SHUTDOWN from: {}", err);
-                break;
-            }
-            State::BridgeShutdown(err) => {
-                error!("SHUTDOWN from: {}", err);
-                break;
-            }
-            State::UpstreamShutdown(err) => {
-                error!("SHUTDOWN from: {}", err);
-                break;
-            }
-            State::Healthy(msg) => {
-                info!("HEALTHY message: {}", msg);
-            }
-        }
-    }
+    let _ = task_collector.safe_lock(|t| t.push((task.abort_handle(), "init task".to_string())));
 }
